81d54c65fd5452c30643524efacbf010677854f8
doanngochuy94@gmail.com <doanngochuy94@gmail.com>
2024-03-23 21:05:23 +0700

    second commit

--- code.ts
+++ code.ts
@@ -1,59 +1,19 @@
-
-async function importNode(nodeKeyArray: string[]) {
-  const importedNode: SceneNode[] = [];
-  nodeKeyArray.forEach(nodeKey => {
-    async function importByKey(key: string) {
-      const importedFrame = (await figma.importComponentByKeyAsync(key)).clone();
-      importedNode.push(importedFrame)
-      return importedNode
-    }
-    importByKey(nodeKey);
-  });
-
-  const setLayoutProps = (frame: FrameNode) => {
-    frame.name = 'Frameee';
-    frame.layoutMode = "VERTICAL";
-    frame.paddingTop = 10;
-    frame.paddingRight = 10;
-    frame.paddingBottom = 10;
-    frame.paddingLeft = 10;
-    frame.itemSpacing = 160;
-  };
-
-  if (importedNode.length > 1) {
-    let parentConteiner = importedNode[0].parent;
-    if (parentConteiner) {
-      let frame = figma.createFrame();
-      const group = figma.group(importedNode, parentConteiner);
-      frame.x = group.x;
-      frame.y = group.y;
-      frame.backgrounds = [];
-      frame.resize(group.width, group.height);
-      setLayoutProps(frame);
-      parentConteiner.appendChild(frame);
-      figma.currentPage.selection = [frame];
-    }
-  }
-
-
-
-
-
-
-
-}
-
-
-
-
-
-figma.showUI(__html__, { title: 'Banner Ads Generator', height: 504, width: 336 });
+figma.showUI(__html__, { title: 'Get frame keys as JSON', height: 504, width: 336 });
 figma.ui.onmessage = msg => {
   if (msg.type === 'create-banner-ad') {
-    console.log('i was here');
-    importNode(msg.selectedOptions);
-    
+    const allNode = figma.root.findAll();
+    const page = figma.currentPage;
+    // const nodes = page.children as ComponentNode[];
+   
+    const instanceNode = allNode.filter(node => { return node.type === "COMPONENT" && node.parent && node.parent.type === "PAGE" }) as ComponentNode[];
 
+    const framesData = instanceNode.map(node => ({
+      category: page.name,
+      name: node.name,
+      key: node.key,
+    }));
+    //console.log(framesData);
+    figma.ui.postMessage({  type: 'framesData', data: framesData })
   }
 }
 
